3dfb6bb42b4b43622ef2bb8425511d2b1a13f680
ChJa2024 <christerfer.james@gmail.com>
2024-12-14 16:37:31 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -19,3 +19,46 @@ var checkDead = setInterval (function(){
         alert ("You Lose!\nPlease Return to Home Page");
     }
 },10);
+
+	//Validtion Code For Inputs
+
+    var user = document.forms['form']['user'];
+    var zip = document.forms['form']['zip'];
+    
+    var user_error = document.getElementById('user_error');
+    var zip_error = document.getElementById('zip_error');
+    
+    user.addEventListener('textInput', user_Verify);
+    zip.addEventListener('textInput', zip_Verify);
+    
+    // Validation code
+    function validated(){
+        if (user.value.length < 20) {
+            user.style.border = "1px solid red";
+            user_error.style.display = "block";
+            user.focus();
+            return false;
+        }
+        if (zip.value.length < 5) {
+            zip.style.border = "1px solid red";
+            zip_error.style.display = "block";
+            zip.focus();
+            return false;
+        }
+    
+    }
+    // verify correct user name and password
+    function user_Verify(){
+        if (user.value.length >= 20) {
+            user.style.border = "1px solid silver";
+            user_error.style.display = "none";
+            return true;
+        }
+    }
+    function zip_Verify(){
+        if (zip.value.length >= 5) {
+            zip.style.border = "1px solid silver";
+            zip_error.style.display = "none";
+            return true;
+        }
+    }
